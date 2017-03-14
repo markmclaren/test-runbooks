dc26f9f52ef2a9161a9cd7ae0abdb7e782ef8193
Mark McLaren <mark.mclaren@gmail.com>
2017-03-14 13:53:49 +0000

    Updated runbook1.ps1 in Azure Automation.

--- runbooks/runbook1.ps1
+++ runbooks/runbook1.ps1
@@ -6,7 +6,6 @@ Get-Mailbox -ResultSize unlimited
 #>
 #Import-Module -Name ExchangeModule #-Verbose
 #$myCredential = Get-AutomationPSCredential -Name 'MyCredential'
-#$myCredential = Get-AutomationPSCredential -Name 'MyCredential'
 #$userName = $myCredential.UserName
 #$securePassword = $myCredential.Password
 #$password = $myCredential.GetNetworkCredential().Password
@@ -20,8 +19,14 @@ Get-Mailbox -ResultSize unlimited
 
 # Pull credential from Automation assets
 $credObject = Get-AutomationPSCredential -Name "MyCredential"
+$connectionUrl = "https://ps.outlook.com/PowerShell-LiveID"
+#$connectionUrl = "https://outlook.office365.com/powershell-liveid/"
  
 #Connect to Exchange Online
-$ExchangeOnlineSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/PowerShell-LiveID -Credential $credObject -Authentication Basic -AllowRedirection
+$ExchangeOnlineSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $connectionUrl -Credential $credObject -Authentication Basic -AllowRedirection
 Import-Module (Import-PSSession -Session $ExchangeOnlineSession -AllowClobber -DisableNameChecking) -Global
-Get-Mailbox -ResultSize unlimited
+Get-Mailbox -ResultSize unlimited | Format-Table 
+Get-PSSession | Format-Table 
+
+
+Write-Output "Completed."
