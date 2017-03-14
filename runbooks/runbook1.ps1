52309415d2a9eaecc808e5904f4a4afeb73b8cf0
Mark McLaren <mark.mclaren@gmail.com>
2017-03-14 11:28:44 +0000

    Updated runbook1.ps1 in Azure Automation.

--- runbooks/runbook1.ps1
+++ runbooks/runbook1.ps1
@@ -1,7 +1,10 @@
-Get-Module -ListAvailable
 <#
 Get-Process
 $myCredential = Get-AutomationPSCredential -Name 'MyCredential'
 Set-ModuleCredential -Cred $myCredential -Conn "https://ps.outlook.com/PowerShell-LiveID?PSVersion=5.1.14393.187" 
 Get-Mailbox -ResultSize unlimited
 #>
+Import-Module -Name ExchangeModule -Verbose
+$myCredential = Get-AutomationPSCredential -Name 'MyCredential'
+Set-ModuleCredential -Cred $myCredential -Conn "https://ps.outlook.com/PowerShell-LiveID?PSVersion=5.1.14393.187" 
+Get-Mailbox -ResultSize unlimited
